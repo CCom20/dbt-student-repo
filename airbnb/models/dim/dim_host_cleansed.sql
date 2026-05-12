46aaec08c60c47cb2b2bcb65e004a2483a9a27d0
Cole Comstock <cole.d.comstock@gmail.com>
2026-05-12 15:18:42 -0600

    custom test added

--- airbnb/models/dim/dim_host_cleansed.sql
+++ airbnb/models/dim/dim_host_cleansed.sql
@@ -2,7 +2,7 @@ WITH dim_hosts_cleansed AS (
     SELECT * FROM {{ ref('src_hosts') }}
 )
 SELECT host_id
-    , NULLIF(host_name, 'Anonymous') as host_name
+    , NVL(host_name, 'Anonymous') as host_name
     , is_superhost
     , created_at
     , updated_at
