dd40776567ebdc0c01f6ee2edec609cbba6b34cd
Cole Comstock <cole.d.comstock@gmail.com>
2026-05-12 12:22:43 -0600

    testing a push

--- airbnb/models/fct/fct_reviews.sql
+++ airbnb/models/fct/fct_reviews.sql
@@ -1,6 +1,15 @@
+{{
+    config(
+        materialized = 'incremental',
+        on_schema_change='fail'
+    )
+}}
 WITH fct_reviews AS (
-    select * from {{ ref(src_reviews) }}
-    
+    select * from {{ ref('src_reviews') }}
+
 )
 select * from fct_reviews
 where review_text is not null
+{% if is_incremental() %}
+    AND review_date > (select max(review_date) from {{ this }})
+{% endif %}
